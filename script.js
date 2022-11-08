cff7990b9b0b6f254fed3b56defe530862acc0db
lfalcaolopes <lfalcaolopes@gmail.com>
2022-11-08 11:26:31 -0300

    Implement tab navigation

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-x = document.querySelectorAll("p")
+document.documentElement.className += " js"
 
-console.log(x[-1])
+tabMenu = document.querySelectorAll(".js-tabmenu li")
+tabcontent = document.querySelectorAll(".js-tabcontent section")
+
+console.log(tabMenu)
+console.log(tabcontent)
+tabcontent[0].classList.add("ativo")
+
+tabMenu.forEach((element, index) => {
+  element.addEventListener("click", function(){
+    tabcontent.forEach((element) => {
+      element.classList.remove("ativo")
+    })
+
+    tabcontent[index].classList.add("ativo")
+  })
+});
