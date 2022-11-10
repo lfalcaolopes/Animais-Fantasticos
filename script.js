ac3b5fa51cf35682798778b3115fa3ed11fe758d
lfalcaolopes <lfalcaolopes@gmail.com>
2022-11-10 14:54:01 -0300

    Add animations

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function initTabNav(){
   tabMenu = document.querySelectorAll(".js-tabmenu li")
   tabcontent = document.querySelectorAll(".js-tabcontent section")
   
-  console.log(tabMenu)
-  console.log(tabcontent)
   tabcontent[0].classList.add("ativo")
   
   tabMenu.forEach((element, index) => {
@@ -23,12 +21,44 @@ function initAccordionNav(){
   faqMenu = document.querySelectorAll(".js-faq dt")
   faqContent = document.querySelectorAll(".js-faq dd")
   
-  console.log(faqMenu)
-  console.log(faqContent)
-  
   faqMenu.forEach((item, index)=>{
     item.addEventListener("click", function(){
       faqContent[index].classList.toggle("ativo")
+      faqMenu[index].classList.toggle("ativo")
+    })
+  })
+}
+
+function smoothTabScroll(){
+  menuItens = document.querySelectorAll(".js-menu [href^='#']")
+
+  menuItens.forEach((item)=>{
+    item.addEventListener('click', function(event){
+      event.preventDefault()
+      id = this.getAttribute("href")
+
+      section = document.querySelector(id)
+
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  })
+}
+
+function scrollAnimation(){
+  sections = document.querySelectorAll(".js .js-scroll")
+
+  sections[0].classList.add("ativo")
+
+  window.addEventListener('scroll', ()=>{
+    sections.forEach((section)=>{
+      const top = section.getBoundingClientRect().top - (window.innerHeight * 0.7)
+
+      if(top < 0){
+        section.classList.add("ativo")
+      }
     })
   })
 }
@@ -41,9 +71,7 @@ function initAccordionNav(){
 
 
 
-
-
-
-
 initTabNav()
 initAccordionNav()
+smoothTabScroll()
+scrollAnimation()
