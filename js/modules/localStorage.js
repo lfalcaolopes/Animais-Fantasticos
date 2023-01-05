de2e4bbe52ccd87898e4dedce950ea6a9c49a491
lfalcaolopes <lfalcaolopes@gmail.com>
2023-01-05 00:12:26 -0300

    Create local storage integration

---
+++ js/modules/localStorage.js
@@ -0,0 +1,15 @@
+export default function localStorage(){
+  const notes = document.querySelector("#notes");
+
+  notes.addEventListener('keyup', (event) => {
+    //Save text from textArea on local storage
+    window.localStorage.setItem('notes', document.forms[0].notes.value)
+  })
+
+  //get any text already saved and append on textArea
+  if(window.localStorage.notes != undefined){
+    notes.value = window.localStorage.notes
+  }
+  
+
+}
--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ import initModal from "./modules/modal.js";
 import initDropdownMenu from "./modules/dropdownMenu.js";
 import initNumCounter from "./modules/numCounter.js";
 import initOperatingHours from "./modules/operatingHours.js";
+import initLocalStorage from "./modules/localStorage.js";
 
 //Verification to only add js animations if browser supports it
 document.documentElement.className += " js";
@@ -33,3 +34,6 @@ initNumCounter();
 
 //Operating Hours visual confirmation
 initOperatingHours();
+
+//Store notes on local storage
+initLocalStorage();
